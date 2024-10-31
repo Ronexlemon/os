76cfb2cb82d443a081cae1c593e71c43e84767e3
RonexLemon <ronexondimu@gmail.com>
2024-10-31 16:18:00 +0300

    change mod

--- chmod/chmod.go
+++ chmod/chmod.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"fmt"
+	"io/fs"
+	"os"
+)
+
+func changeMode(){
+	err:=os.Chmod("chmod.go",fs.ModeAppend)
+	if err !=nil{
+		fmt.Println("change mod error",err)
+	}
+}
+
+func main(){
+	changeMode()
+}
